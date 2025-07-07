ee9d15a925b550a425a879f4d1e38fce808077fa
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-07 17:38:39 +0300

    feat: add simple http server setup
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
-	@g++ main.cpp -o bin/main
+	@g++ main.cpp -std=c++17 -Iinclude -o bin/main
 
 run:
 	@./bin/main
+
+deploy:
+	@ngrok http http://localhost:8080
--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include "httplib.h"
+#include "inja.hpp"
+#include "json.hpp"
+
+using namespace std;
+using namespace httplib;
+using json = nlohmann::json;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Server svr;
+    inja::Environment env;
+    string html_template = env.load_file("templates/index.html");
+
+    svr.Get("/", [=, &env](const Request& req, Response& res) {
+        json data;
+        data["title"] = "Inja Example";
+        data["name"] = "Ilia Petrov";
+
+        string result = env.render(html_template, data);
+        res.set_content(result, "text/html");
+    });
+
+    cout << "Server is running on port 8080" << endl;
+    svr.listen("localhost", 8080);
+    return 0;	
 }
